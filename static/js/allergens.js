6043baea9c84b4dec74c5e4a2c66f05d3c5efa74
J4CKINS <electronicjack4@gmail.com>
2024-08-17 21:07:38 +0100

    Created allergen data.

---
+++ static/js/allergens.js
@@ -0,0 +1,19 @@
+
+const allergen = {
+    Celery : "Celery",
+    Lupin : "Lupin",
+    Eggs : "Eggs",
+    Fish : "Fish",
+    Soya : "Soya",
+    Milk : "Milk",
+    Mustard : "Mustard",
+    Peanuts : "Peanuts",
+    Sesame : "Sesame",
+    Crustacea : "Crustacea",
+    Molluscs : "Molluscs",
+    Nuts : "Nuts",
+    Gluten : "Gluten",
+    Sulphites : "Sulphites",
+    Vegetarian : "Vegetarian",
+    Vegan : "Vegan"
+}
---
+++ static/js/icecream.js
@@ -0,0 +1,242 @@
+const icecream = [
+    {
+        name: "Cornish Vanilla",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 149
+    },
+    {
+        name: "Clotted Cream Vanilla Bean Seed",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 102
+    },
+    {
+        name: "Strawberry",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 100
+    },
+    {
+        name: "Chocolate",
+        contains: [
+            allergen.Soya,
+            allergen.Milk
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 121
+    },
+    {
+        name: "Mint Choc Chip",
+        contains: [
+            allergen.Soya,
+            allergen.Milk
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 117
+    },
+    {
+        name: "Rum & Raisin",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 108
+    },
+    {
+        name: "Lemon Crunch",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 121
+    },
+    {
+        name: "Honeycomb Caramel Swirl",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 119
+    },
+    {
+        name: "Clotted Cream & Raspberry Ripple",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 111
+    },
+    {
+        name: "Millionaires Shortbread",
+        contains: [
+            allergen.Soya,
+            allergen.Milk,
+            allergen.Gluten
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 136
+    },
+    {
+        name: "Rocky Road",
+        contains: [
+            allergen.Soya,
+            allergen.Milk,
+            allergen.Gluten
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: false,
+        vegan: false,
+        kcal: 147
+    },
+    {
+        name: "Bubblegum",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 105
+    },
+    {
+        name: "Caffe Latte",
+        contains: [
+            allergen.Soya,
+            allergen.Milk
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 108
+    },
+    {
+        name: "Candy Floss",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 114
+    },
+    {
+        name: "Cookies & Cream",
+        contains: [
+            allergen.Soya,
+            allergen.Milk,
+            allergen.Gluten
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 120
+    },
+    {
+        name: "Eton Mess",
+        contains: [
+            allergen.Eggs,
+            allergen.Milk,
+            allergen.Gluten
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 126
+    },
+    {
+        name: "Toffee Fudge",
+        contains: [
+            allergen.Soya,
+            allergen.Milk
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 110
+    },
+    {
+        name: "Banana Split",
+        continue: [
+            allergen.Soya,
+            allergen.Milk,
+            allergen.Nuts
+        ],
+        may_contain: [],
+        vegetarian: true,
+        vegan: false,
+        kcal: 120
+    },
+    {
+        name: "Salted Caramel",
+        contains: [
+            allergen.Soya,
+            allergen.Milk
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 126
+    },
+    {
+        name: "Blueberry",
+        contains: [
+            allergen.Soya,
+            allergen.Milk
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 123
+    },
+    {
+        name: "Vegan Vanilla",
+        contains: [ allergen.Soya ],
+        may_contain: [
+            allergen.Milk,
+            allergen.Nuts
+        ],
+        vegetarian: true,
+        vegan: true,
+        kcal: 89
+    },
+    {
+        name: "Mango",
+        contains: [
+            allergen.Soya,
+            allergen.Milk
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 113
+    },
+    {
+        name: "Cherry",
+        contains: [ allergen.Milk ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 117
+    },
+    {
+        name: "Coconut Choc Cookie",
+        contains: [
+            allergen.Soya,
+            allergen.Milk,
+            allergen.Gluten
+        ],
+        may_contain: [ allergen.Nuts ],
+        vegetarian: true,
+        vegan: false,
+        kcal: 124
+    }
+]
